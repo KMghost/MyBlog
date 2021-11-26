a0167ba1665ef38eefb031a50a55bbc3a8f4c1e8
Daniel <wuyh512@foxmail.com>
2021-11-26 23:48:48 +0800

    路由封装

--- src/utils/api.js
+++ src/utils/api.js
@@ -26,3 +26,12 @@ axios.interceptors.response.use(success => {
     }
     return;
 })
+
+const base = ''
+export const postRequest = (url, params) => {
+    return axios({
+        method: 'post',
+        url: `${base}${url}`,
+        data: params
+    })
+}
