ea92307a7399fba222a0c4f24f44be3e124461f9
Daniel <wuyh512@foxmail.com>
2021-11-26 15:42:05 +0800

    引入 elementUI

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import router from 'router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    render: h => h(App)
 })
---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter)
+
+export default new Router({
+    mode: 'history',  // mode两种模式 hash是路径带#的，mode是路径不带#的
+    routes: [
+        {
+
+        }
+    ]
+})
