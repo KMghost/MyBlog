a47c818e663ab86aa6469985e5cd0e612489a1cf
Daniel <wuyh512@foxmail.com>
2022-01-15 10:48:59 +0800

    根据 id 删除角色菜单，新增角色菜单

--- src/components/basic/PermissMana.vue
+++ src/components/basic/PermissMana.vue
@@ -4,8 +4,9 @@
 			<el-input size="small" placeholder="请输入角色英文名" v-model="role.name">
 				<template slot="prepend">ROLE_</template>
 			</el-input>
-			<el-input size="small" v-model="role.nameZh" placeholder="请输入角色中文名"></el-input>
-			<el-button size="small" type="primary" icon="el-icon-plus">添加角色</el-button>
+			<!--@keydown.enter.native 增加键盘回车事件-->
+			<el-input size="small" v-model="role.nameZh" placeholder="请输入角色中文名" @keydown.enter.native="doAddRole"></el-input>
+			<el-button size="small" type="primary" icon="el-icon-plus" @click="doAddRole">添加角色</el-button>
 		</div>
 		<div class="permissManaMain">
 			<el-collapse v-model="activeName" accordion @change="change">
@@ -14,13 +15,14 @@
 						<div slot="header" class="clearfix">
 							<span>可访问资源</span>
 							<el-button style="float: right; padding: 3px 0; color: #ff0000" type="text"
-									   icon="el-icon-delete"></el-button>
+									   icon="el-icon-delete" @click="doDeleteRole(r)"></el-button>
 						</div>
 						<div>
 							<el-tree show-checkbox
 									 :data="allMenus"
 									 :props="defaultProps"
 									 ref="tree"
+									 :key="index"
 									 :default-checked-keys="selectedMenus"
 									 node-key="id"></el-tree>
 							<div style="display: flex;justify-content: flex-end">
@@ -58,6 +60,32 @@ export default {
 		this.initRoles();
 	},
 	methods: {
+		doDeleteRole(role){
+			this.$confirm('此操作将永久删除[' + role.nameZh + ']角色, 是否继续 ? ', '提示', {
+						confirmButtonText: '确定',
+						cancelButtonText: '取消',
+						type: 'warning'
+					}
+			).then(() => {
+				this.deleteRequest('/system/basic/permiss/role/'+role.id).then(resp => {
+					if (resp) {
+						this.initRoles();
+					}
+				})
+			}).catch(() => {
+			});
+		},
+		doAddRole(){
+			if (this.role.name && this.role.nameZh) {
+				this.postRequest('/system/basic/permiss/role',this.role).then(resp=>{
+					this.initRoles();
+					this.role.name = '';
+
+				})
+			}	else {
+				this.$message.error('所有字段不能为空！');
+			}
+		},
 		cancelUpdate(){
 			console.log(this.activeName)
 			this.activeName = '-1'
