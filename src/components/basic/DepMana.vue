4d08035fe2570bb9704a67273e26484583426f10
Daniel <wuyh512@foxmail.com>
2022-01-28 17:39:57 +0800

    部门展示与搜索

--- src/components/basic/DepMana.vue
+++ src/components/basic/DepMana.vue
@@ -1,10 +1,59 @@
 <template>
-	<div>部门管理</div>
+	<div style="width: 500px">
+		<el-input
+				placeholder="请输入部门名称进行搜索..."
+				prefix-icon="el-icon-search"
+				v-model="filterText">
+		</el-input>
+
+		<el-tree
+				:data="deps"
+				:props="defaultProps"
+				:filter-node-method="filterNode"
+				ref="tree">
+		</el-tree>
+	</div>
 </template>
 
 <script>
 export default {
-	name: "DepMana"
+	name: "DepMana",
+	watch: {
+		// 这里的filter 调用的是 filter-node-method
+		// 这里的 val 是输入框中绑定的 filterText
+		filterText(val) {
+			this.$refs.tree.filter(val);
+		}
+	},
+	mounted() {
+		this.initDeps();
+	},
+	methods: {
+		// 如果没有value，就全部展开，如果有值，
+		// data 是当前结点下面的整条数据
+		filterNode(value, data) {
+			if (!value) return true;
+			// 判断 data.name 是不是包含这个值
+			return data.name.indexOf(value) !== -1;
+		},
+		initDeps() {
+			this.getRequest('/system/basic/department/').then(resp => {
+				if (resp) {
+					this.deps = resp;
+				}
+			})
+		}
+	},
+	data() {
+		return {
+			filterText: '',
+			deps: [],
+			defaultProps: {
+				children: 'children',
+				label: 'name'
+			}
+		}
+	}
 }
 </script>
 
