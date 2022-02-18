1823e76332cbbbcdc00512f525e7a5eeed517864
Daniel <wuyh512@foxmail.com>
2022-02-18 23:11:05 +0800

    操作员角色更新功能实现

--- src/views/sys/SysAdmin.vue
+++ src/views/sys/SysAdmin.vue
@@ -38,7 +38,23 @@
 						<el-tag type="success" style="margin-right: 4px" v-for="(role,indexj) in admin.roles"
 								:key="indexj">{{ role.nameZh }}
 						</el-tag>
-						<el-button type="text" icon="el-icon-more"></el-button>
+						<el-popover
+								placement="right"
+								title="角色列表"
+								width="200"
+								@show="showPop(admin)"
+								@hide="hidePop(admin)"
+								trigger="click">
+							<el-select v-model="selectedRoles" multiple placeholder="请选择">
+								<el-option
+										v-for="(r,index) in allRoles"
+										:key="index"
+										:label="r.nameZh"
+										:value="r.id">
+								</el-option>
+							</el-select>
+							<el-button slot="reference" type="text" icon="el-icon-more"></el-button>
+						</el-popover>
 					</div>
 					<div>
 						备注: {{ admin.remark }}
@@ -57,12 +73,65 @@ export default {
 		return {
 			admins: [],
 			keyword: '',
+			allRoles: [],
+			selectedRoles: [],
 		}
 	},
 	mounted() {
 		this.initAdmins();
 	},
 	methods: {
+		hidePop(admin) {
+			let roles = []
+			Object.assign(roles, admin.roles);
+			let flag = false;
+			if (roles.length != this.selectedRoles.length) {
+				flag = true;
+			} else {
+				for (let i = 0; i < roles.length; i++) {
+					let role = roles[i];
+					for (let j = 0; j < this.selectedRoles.length; j++) {
+						let sr = this.selectedRoles[j];
+						if (role.id == sr) {
+							roles.splice(i, 1);
+							i--;
+							break;
+						}
+					}
+				}
+				if (roles.length != 0){
+					flag = true;
+				}
+			}
+
+
+			if (flag == true) {
+				let url = '/system/admin/role?adminId=' + admin.id;
+				this.selectedRoles.forEach(sr => {
+					url += '&rids=' + sr;
+				});
+				this.putRequest(url).then(resp => {
+					if (resp) {
+						this.initAdmins();
+					}
+				})
+			}
+		},
+		showPop(admin) {
+			this.initAllRoles();
+			let roles = admin.roles;
+			this.selectedRoles = [];
+			roles.forEach(r => {
+				this.selectedRoles.push(r.id);
+			})
+		},
+		initAllRoles() {
+			this.getRequest('/system/admin/roles').then(resp => {
+				if (resp) {
+					this.allRoles = resp;
+				}
+			})
+		},
 		enabledChange(admin) {
 			this.putRequest('/system/admin/', admin).then(resp => {
 				if (resp) {
